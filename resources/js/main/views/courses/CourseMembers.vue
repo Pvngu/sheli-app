fdeefbbfd9a8f5c00440a6e695abfa2b2a1e72c7
Pvngu <omargt151102@gmail.com>
2024-12-01 17:11:09 -0800

    Add CourseMembers view for displaying course member avatars

---
+++ resources/js/main/views/courses/CourseMembers.vue
@@ -0,0 +1,29 @@
+<template>
+    <a-avatar-group
+        :max-count="4"
+        :max-style="{
+            color: '#f56a00',
+            backgroundColor: '#fde3cf',
+        }"
+    >
+        <a-tooltip
+            v-for="member in members"
+            :key="member.x_user_id"
+            :title="member.user.name"
+            placement="top"
+        >
+            <a-avatar :src="member.user.profile_image_url" />
+        </a-tooltip>
+    </a-avatar-group>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: ["members"],
+    setup(props, { emit }) {
+        return {};
+    },
+});
+</script>
